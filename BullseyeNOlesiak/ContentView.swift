b1b5c2bc4c75d6bffe150c3b17ce89b5040ffd66
Norbert Olesiak-Zbieg <n.olesiak@gmail.com>
2022-04-09 19:33:13 +0200

    init swift app

--- BullseyeNOlesiak/ContentView.swift
+++ BullseyeNOlesiak/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var alertIsVisible: Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Wyceluj jak najbliżej tej wartości")
+                .bold()
+                .kerning(2.0)
+                .multilineTextAlignment(.center)
+                .padding()
+                .lineSpacing(4.0)
+                .font(.footnote)
+                
+            Text("89")
+                .kerning(-1.0)
+                .font(.largeTitle)
+                .fontWeight(.black)
+            HStack {
+                Text("1")
+                    .bold()
+                Slider(value: .constant(50), in: 1.0...100.0)
+                Text("100")
+                    .bold()
+                    
+            }
+            Button(action: {
+                print("Hit me pressed")
+                self.alertIsVisible = true
+            }) {
+                Text("Hit me")
+            }
+            
+            .alert("Hello there", isPresented: $alertIsVisible){
+                Button("Awsome") {}
+            } message: {
+                Text("My first popup")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView().previewLayout(.fixed(width: 568, height: 320))
     }
 }
