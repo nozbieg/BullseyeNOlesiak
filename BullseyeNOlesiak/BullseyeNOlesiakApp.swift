dc6cc55aaccf230f601cd7c6a405c8bb1886a039
Norbert Olesiak-Zbieg <n.olesiak@gmail.com>
2022-04-09 18:26:47 +0200

    Initial Commit

---
+++ BullseyeNOlesiak/BullseyeNOlesiakApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeNOlesiakApp.swift
+//  BullseyeNOlesiak
+//
+//  Created by Norbert Olesiak-Zbieg on 09/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeNOlesiakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
